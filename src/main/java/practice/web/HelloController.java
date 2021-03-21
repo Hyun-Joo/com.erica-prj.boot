3000a05f88f56d8234e76b9b254fe8bde1987fb4
Hyun-Joo <dlguswn714@gmail.com>
2021-03-21 11:09:28 +0900

    junit test 추가

---
+++ src/main/java/practice/web/HelloController.java
@@ -0,0 +1,21 @@
+package practice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import practice.web.dto.HelloResponseDto;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "HELLO";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
+}
